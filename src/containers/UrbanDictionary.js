addad0a84ddffa4a5829d43bc34ef24bdd7f55f4
Chris Barclay <chris.barclay1@gmail.com>
2023-03-10 20:23:13 +0000

    Can search for results and get an error if no results are returned

--- src/containers/UrbanDictionary.js
+++ src/containers/UrbanDictionary.js
@@ -9,12 +9,20 @@ const UrbanDictionary = ()=>{
     const [next, setNext] = useState(2);
     const [previous, setPrevious] = useState(0);
     const [definedTerm, setDefinedTerm] = useState("JavaScript");
+    const [newSearchText, setNewSearchText] = useState("");
 
     async function fetchArticles(word = definedTerm){
         const res = await fetch(`https://api.urbandictionary.com/v0/define?term=${word}`);
         const data = await res.json();
+        if (data.list.length > 0){
         setArticles(data.list);
         setSelected(data.list[0]);
+        document.getElementById("display").style.display = "block";
+        document.getElementById("noresult").style.display = "none";
+    } else{
+        document.getElementById("display").style.display = "none";
+        document.getElementById("noresult").style.display = "block";
+        }
     }
 
     useEffect(()=>{
@@ -30,20 +38,32 @@ const UrbanDictionary = ()=>{
         setPrevious(value-1);
     };
 
-    const termChange = (word)=>{
-        setDefinedTerm(word);
-        fetchArticles(word);
+    const newSearch= (e)=>{
+        e.preventDefault();
+        setDefinedTerm(newSearchText);
+        fetchArticles(newSearchText);
+        setNewSearchText("");
+        document.getElementById("form").reset()
+    }
+
+    const handleInputChange = (e)=>{
+        setNewSearchText(e.target.value);
     };
 
     return(
         <>
         <h1>Urban Dictionary</h1>
         <h2>Search Term: {definedTerm} </h2>
-        <form>
-            <input type="text" placeholder="Enter a search term"/>
+        <form id="form" onSubmit={newSearch}>
+            <input type="text" placeholder="Enter a search term" onChange={handleInputChange}/>
             <button type="submit">Submit</button>
         </form>
-        <Display selected={selected}/>
+        <div id="display">
+            <Display selected={selected}/>
+        </div>
+        <div id="noresult">
+            <h3>No Results! Please search again...</h3>
+        </div>
         <Button 
         text="Previous"
         isDisabled={previous === 0}
